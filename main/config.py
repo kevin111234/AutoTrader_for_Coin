b7c979e0633f5a09d09802e719376e1666622092
kevin111234 <woghd111234@naver.com>
2024-12-16 15:19:00 +0000

    ✨ "환경변수 불러오는 기능 작성"

--- main/config.py
+++ main/config.py
@@ -1,7 +1,20 @@
+import os
+import dotenv
 
 class Config():
     def __init__(self):
         print("환경변수 로드 중...")
+        binance_access_key = os.getenv("BINANCE_ACCESS_KEY")
+        binance_secret_key = os.getenv("BINANCE_SECRET_KEY")
+        slack_api_key = os.getenv("SLACK_API_KEY")
+        slack_asset_channel_id = os.getenv("SKACK_ASSET_CHANNEL_ID")
+        slack_trade_channel_id = os.getenv("SKACK_TRADE_CHANNEL_ID")
+        slack_error_channel_id = os.getenv("SKACK_ERROR_CHANNEL_ID")
+
+        seed_money = os.getenv("SEED_MONEY")
+        coin_tickers = os.getenv("COIN_TICKERS")
+        sto_fut_rate = os.getenv("STO_FUT_RATE")
+        leverage_rate = os.getenv("LEVERAGE_RATE")
 
         print("환경변수 로드 완료")
         
