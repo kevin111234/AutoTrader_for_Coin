a621896ae3ee2ac210b2f5bd7c6be40959425803
kevin111234 <woghd111234@naver.com>
2024-12-16 09:02:32 +0000

    ✨ "slack 알림 코드 기본흐름 작성"

---
+++ main/notifier.py
@@ -0,0 +1,22 @@
+from config import Config
+class Notifier():
+    def __init__(self):
+        pass
+
+    def get_asset_info():
+        print("자산 정보 불러오기 함수를 실행합니다.")
+
+    def get_limit_amount():
+        print("투자 한도 불러오기 함수를 실행합니다.")
+
+    def send_slack_message():
+        print("slack 메시지 전송 함수를 실행합니다.")
+
+    def send_asset_info():
+        print("자산 정보 전송 함수를 실행합니다.")
+
+    def send_trade_info():
+        print("거래 정보 전송 함수를 실행합니다.")
+
+    def send_error_info():
+        print("에러 정보 전송 함수를 실행합니다.")
