c6ab323aae0477d2bf2e86e6ce319b3c451955e2
kevin111234 <woghd111234@naver.com>
2024-12-17 16:39:53 +0000

    ⚡️ "초기 데이터 조회 삭제, 가격데이터 업데이트 부분 코드 작성"

--- main/main.py
+++ main/main.py
@@ -14,24 +14,18 @@ def main():
 
     # 초기 자산 조회 - notifier.py
 
-    # 초기 데이터 수집 - data_control.py
-    for i in ticker_list:
-        i.split("-")
-        symbol = i[1]+i[0]
-        data_control.data(client, symbol, timeframe)
-
     # 반복문 시작
     while True:
         try:
             print("매수/매도 판단을 시작합니다.")
             # 현재 가격 조회
+
+            # 가격데이터 업데이트
             for i in ticker_list:
                 i.split("-")
                 symbol = i[1]+i[0]
                 timeframe = "5MINUTE"
-                data_control.data(client,symbol,timeframe)
-
-            # 가격데이터 업데이트
+                data = data_control.data(client,symbol,timeframe)
 
             # 매수/매도 판단
 
