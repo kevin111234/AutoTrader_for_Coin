e4a7db9043913bcb66a74e5d2386a8cf6e88774f
kevin111234 <woghd111234@hanyang.ac.kr>
2025-01-16 14:32:07 +0000

    🔥 "안쓰는 함수 삭제"

--- src/strategy.py
+++ src/strategy.py
@@ -12,41 +12,6 @@ class Strategy():
         print("매수이면 포지션 정보 기록")
         print("매도이면 포지션 정보 삭제")
 
-    def rsi_check(self):
-        print("RSI 체크 함수 실행")
-
-    def volume_profile(self):
-        print("볼륨 프로파일 체크 함수 실행")
-
-        score = int()
-        
-        return score
-
-    def OBV_check(self):
-        print("OBV 체크 함수 실행")
-
-        score = int()
-
-        return score
-    
-    def bandwidth_check(self):
-        print("볼린저밴드 폭 체크 함수 실행")
-
-        score = int()
-
-        return score
-
-    def volatility_score(self):
-        print("변동성 집계 함수 실행")
-
-        vp_score = self.volume_profile(self)
-        OBV_score = self.OBV_check(self)
-        bw_score = self.bandwidth_check(self)
-
-        total_score = int()
-
-        return total_score
-
     def buy_signal(self):
         
         signal = bool()
