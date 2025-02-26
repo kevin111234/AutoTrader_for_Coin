2c13866afbc46fe5332a614e6aae21c2e1c43a71
kevin111234 <woghd111234@hanyang.ac.kr>
2025-02-26 08:01:18 +0000

    🐛 "디렉토리 절대경로 지정"

--- src/notifier.py
+++ src/notifier.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# 프로젝트 루트 디렉토리의 절대 경로를 구함
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
 from src.config import Config
 from binance.client import Client
 from slack_sdk import WebClient
